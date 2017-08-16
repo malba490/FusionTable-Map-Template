9c66e0835e3a254c8dc66e5348118b629bc0f2c1
malba490 <malba490@gmail.com>
2017-08-16 15:37:48 -0400

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -94,7 +94,7 @@
                 where: whereClause
             },
             styleId: 2,
-            templateId: 3
+            templateId: 2
         });
         self.fusionTable = self.searchrecords;
         self.searchrecords.setMap(map);
